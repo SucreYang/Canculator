7faf830f2af8e0da25c6b71853be66376f63c919
SucreYang <yangmumu199104@gmail.com>
2017-12-19 15:10:51 +0800

    更新
    调整数值

--- Canculator/ViewController.swift
+++ Canculator/ViewController.swift
@@ -20,6 +20,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+  
+    @IBOutlet weak var textAre: UILabel!
+    @IBOutlet weak var ew: UIProgressView!
+    var switchOnoff: Bool = true
+    var huakuaiValue: Float = 0.51
+    
+    var theOne = 0
+    
+    @IBAction func addOne(_ sender: UIButton) {
+        theOne += 1
+        textAre.text = "\(theOne)"
+    }
+   
+    @IBAction func switchValue(_ sender: UISwitch) {
+        if sender.isOn {
+            switchOnoff = sender.isOn
+            getProgressValue(sender: huakuaiValue)
+        } else {
+            switchOnoff = false
+            getProgressValue(sender: 0.0)
+        }
+    }
+    
+    @IBAction func huakuai(_ sender: UISlider) {
+        let baifenbi = sender.value / sender.maximumValue * 100
+        theOne = Int(baifenbi)
+        textAre.text = "almost \(Int(baifenbi))%"
+        getResult(sender: "\(Int(baifenbi))")
+        huakuaiValue = sender.value
+        if switchOnoff {
+        getProgressValue(sender: sender.value)
+        } else {
+            getProgressValue(sender: sender.minimumValue)
+        }
+        
+    }
+   
+    func getResult(sender: String) {
+        textAre.text = sender
+    }
+    
+    func getProgressValue(sender: Float) {
+        ew.progress = sender
+    }
 }
 
